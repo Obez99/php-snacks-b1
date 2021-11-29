9b8f7671b085af01d6f035bc357aeee6a2345460
Obez99 <giuliogx@live.it>
2021-11-29 17:53:41 +0100

    Prints random numbers in html

--- Snack3.php
+++ Snack3.php
@@ -13,6 +13,26 @@ for ($i = 0; $i < 15; $i++) {
     array_push($randomNumbers, $randomNumber);
   }
 }
+?>
 
+<!DOCTYPE html>
+<html lang="en">
 
-var_dump($randomNumbers);
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Snack3</title>
+</head>
+
+<body>
+  <ul>
+    <?php
+    for ($i = 0; $i < count($randomNumbers); $i++) {
+      echo "<li>" . $randomNumbers[$i] . "</li>";
+    }
+    ?>
+  </ul>
+</body>
+
+</html>
