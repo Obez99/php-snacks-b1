33dc84723bf717a06530cab314ed1c6b9df1c9b6
Obez99 <giuliogx@live.it>
2021-11-29 14:17:26 +0100

    Adds requested login checks

--- Snack1.php
+++ Snack1.php
@@ -2,15 +2,19 @@
 
 //Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
 //che name sia più lungo di 3 caratteri,
-//che mail contenga un punto e una chiocciola e che age sia un numero.
+//che mail contenga un punto e una chiocciola,
+//che age sia un numero.
 //Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
 if (count($_GET) < 3)
   exit("Inserisci i valori 'name', 'email, 'age'");
+
 
 $name = $_GET["name"];
 $email = $_GET["email"];
 $age = $_GET["age"];
 
-
-echo var_dump($_GET);
-echo var_dump($name, $email, $age);
+if (strlen($name) > 3 && strpos($email, "@") && strpos($email, ".") && intval($age))
+  echo ("Accesso riuscito");
+else
+  echo ("Accesso negato");
